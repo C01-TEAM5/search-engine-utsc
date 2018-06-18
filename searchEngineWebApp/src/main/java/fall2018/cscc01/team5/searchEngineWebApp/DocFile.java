994f6b9e98ac69db1b91b61d48685b9a62c41f36
Andrew <argerrard@live.com>
2018-06-18 14:52:22 -0400

    Created DocFile class and created stub methods for IndexHandler

---
+++ searchEngineWebApp/src/main/java/fall2018/cscc01/team5/searchEngineWebApp/DocFile.java
@@ -0,0 +1,15 @@
+package fall2018.cscc01.team5.searchEngineWebApp;
+
+public class DocFile {
+	
+	/* A DocFile is an object representing a file in the system.
+	 * DocFiles contain a path to where they are stored, can be
+	 * private or public depending on user preferences and can
+	 * be connected to certain UTSC Courses.
+	 * 
+	 */
+	public DocFile(String filename) {
+		
+	}
+
+}
