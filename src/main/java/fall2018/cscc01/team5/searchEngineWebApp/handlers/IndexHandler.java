c9aa9ab7306c6f090e028e46046d9e134330fa08
Kalindu De Costa <kalindu.decosta@mail.utoronto.ca>
2018-06-21 19:39:31 -0400

    TEAM5-6 removed some TAB characters

--- src/main/java/fall2018/cscc01/team5/searchEngineWebApp/handlers/IndexHandler.java
+++ src/main/java/fall2018/cscc01/team5/searchEngineWebApp/handlers/IndexHandler.java
@@ -3,6 +3,7 @@ package fall2018.cscc01.team5.searchEngineWebApp.handlers;
 import fall2018.cscc01.team5.searchEngineWebApp.docs.DocFile;
 import fall2018.cscc01.team5.searchEngineWebApp.util.Constants;
 import fall2018.cscc01.team5.searchEngineWebApp.util.ContentGenerator;
+
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -16,56 +17,53 @@ import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 
-public class IndexHandler{
-	
+public class IndexHandler {
+
     private StandardAnalyzer analyzer;  // Use default setting
     private Directory ramIndex;    //store index in RAM
-    private String storePath;	//The path where the index will be stored
+    private String storePath;    //The path where the index will be stored
 
-	
+
     /**
-     * Construct a new IndexHandler.
-     * This class represents the indexer for the search engine.
-     * Index is stored in RAM.
+     * Construct a new IndexHandler. This class represents the indexer for the search engine. Index is stored in RAM.
      */
-    public IndexHandler(String storedPath) {
+    public IndexHandler (String storedPath) {
         analyzer = new StandardAnalyzer();
         ramIndex = new RAMDirectory();
         storePath = storedPath;
     }
-	
-    /** 
-     * Takes a DocFile as a parameter and adds the contents of the DocFile
-     * to the index. If an invalid document type is passed in, nothing happens.
-     * 
-     * File types that can be passed in include:
-     * .txt, .pdf, .html, .docx
-     * 
+    
+    /**
+     * Takes a DocFile as a parameter and adds the contents of the DocFile to the index. If an invalid document type is
+     * passed in, nothing happens.
+     * <p>
+     * File types that can be passed in include: .txt, .pdf, .html, .docx
+     * <p>
      * Precondition: DocFiles passed into addDoc must not be already indexed
-     * 
+     *
      * @param newFile the object of the file that will be added to the index
      */
-    public void addDoc(DocFile newFile) {
-		
+    public void addDoc (DocFile newFile) {
+
         //Check if the file extension is valid
         if (!isValid(newFile)) {
             return;
         }
-		
+
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
 
         //Create the new document, add in DocID fields and UploaderID fields
         Document newDocument = new Document();
         String filePath = newFile.getPath();
         Field docIDField = new TextField(Constants.INDEX_KEY_PATH, filePath, Store.YES);
-        Field userIDField = new TextField(Constants.INDEX_KEY_OWNER,newFile.getOwner(),Store.YES);
-		
+        Field userIDField = new TextField(Constants.INDEX_KEY_OWNER, newFile.getOwner(), Store.YES);
+
         newDocument.add(docIDField);
         newDocument.add(userIDField);
-		
+
         //Call Content Generator to add in the ContentField
         ContentGenerator.generateContent(newDocument, newFile);
-		
+
         //Add the Document to the Index
         try {
             IndexWriter writer = new IndexWriter(ramIndex, config);
@@ -75,39 +73,37 @@ public class IndexHandler{
             e.printStackTrace();
         }
     }
-	
+
     /**
-     *  Checks to see if a DocFile is valid. A valid DocFile is any file with
-     * the following extensions: .pdf, .txt, .docx, .html
-     * 
+     * Checks to see if a DocFile is valid. A valid DocFile is any file with the following extensions: .pdf, .txt,
+     * .docx, .html
+     *
      * @param file the DocFile to check validity of
      * @return boolean true if the DocFile is a valid extension
      */
-    private static boolean isValid(DocFile file) {
-		
+    private static boolean isValid (DocFile file) {
+
         return Arrays.asList(Constants.VALIDDOCTYPES).contains(file.getFileType());
-		
-	}
-	
+
+    }
+
     /**
-     * Update the indexing of a file that is currently indexed.
-     * This method is called when a document is changed and needs to
-     * be re-indexed.
-     * 
+     * Update the indexing of a file that is currently indexed. This method is called when a document is changed and
+     * needs to be re-indexed.
+     *
      * @param updatefile the object of the file to be updated.
      */
-    public static void updateDoc(DocFile updatefile) {
+    public static void updateDoc (DocFile updatefile) {
         //TODO
     }
-	
-    /** 
-     * Remove the specified document from the index.
-     * If the document has not been indexed, nothing happens.
-     * 
+
+    /**
+     * Remove the specified document from the index. If the document has not been indexed, nothing happens.
+     *
      * @param deletefile the object of the file to be removed from the index
      */
-    public void removeDoc(DocFile deletefile) {
-		
+    public void removeDoc (DocFile deletefile) {
+
     }
 
 }
