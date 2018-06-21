a1c7980c9ee92201cebc37ae114cdca9d88883ba
Kalindu De Costa <kalindu.decosta@mail.utoronto.ca>
2018-06-21 18:50:22 -0400

    TEAM5-6 created constants and replaced all magic values with constants

--- searchEngineWebApp/src/main/java/fall2018/cscc01/team5/searchEngineWebApp/util/Constants.java
+++ searchEngineWebApp/src/main/java/fall2018/cscc01/team5/searchEngineWebApp/util/Constants.java
@@ -2,4 +2,14 @@ package fall2018.cscc01.team5.searchEngineWebApp.util;
 
 public class Constants {
 
+    public static final String FILETYPE_DOCX = "docx";
+    public static final String FILETYPE_HTML = "html";
+    public static final String FILETYPE_PDF = "pdf";
+    public static final String FILETYPE_TXT = "txt";
+
+    public static final String INDEX_KEY_CONTENT = "Content";
+    public static final String INDEX_KEY_OWNER = "Owner";
+    public static final String INDEX_KEY_PATH = "Path";
+
+    public static final String[] VALIDDOCTYPES = {"pdf", "txt", "html", "docx"};
 }
