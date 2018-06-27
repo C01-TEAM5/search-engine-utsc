34beca657206c9a47ecd537f0b7394c702996c49
Andrew Gerrard <andrew.gerrard@mail.utoronto.ca>
2018-06-27 16:51:50 -0400

    TEAM5-55 Started working on main.js, finished off index page

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -8,7 +8,30 @@
         // also it will synchrize the frontend, html files
 
         // use this file for the controls of the search, buttons api calls, redirects, etc.
+        const submit = document.getElementById("submit");
+        const search = document.getElementById("search");
+        const searchTxt = document.getElementById("searchTxt");
+        const searchPdf = document.getElementById("searchPdf");
+        const searchHtml = document.getElementById("searchHtml");
+        const searchDocx = document.getElementById("searchDocx");
 
+        function submitter (event) {
+
+            event.preventDefault();
+
+            var filterList = [];
+
+            const searchQuery = search.value;
+            if (searchTxt.checked) {filterList.push("sTxt")}
+            if (searchPdf.checked) {filterList.push("sPdf")}
+            if (searchHtml.checked) {filterList.push("sHtml")}
+            if (searchDocx.checked) {filterList.push("sDocx")}
+            api.search(searchQuery, filterList);
+
+
+        }
+
+        submit.addEventListener('click', submitter);
 
     };
 
