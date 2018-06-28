4e2b53fb4de1930a86a3be770a3c10fd54b9db35
Andrew <argerrard@live.com>
2018-06-28 16:30:09 -0400

    Started work on search servlet returning results

--- src/main/java/fall2018/cscc01/team5/searchEngineWebApp/servlets/SearchServlet.java
+++ src/main/java/fall2018/cscc01/team5/searchEngineWebApp/servlets/SearchServlet.java
@@ -7,8 +7,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.lucene.queryparser.classic.ParseException;
+
+import com.google.gson.Gson;
+
+import fall2018.cscc01.team5.searchEngineWebApp.docs.DocFile;
+import fall2018.cscc01.team5.searchEngineWebApp.handlers.IndexHandler;
+import fall2018.cscc01.team5.searchEngineWebApp.util.Constants;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class SearchServlet extends HttpServlet {
 
@@ -16,22 +25,95 @@ public class SearchServlet extends HttpServlet {
 
     @Override
     protected void doGet (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/plain");
-        resp.getWriter().write("Show search result.");   // delete
         
-        // TODO: get query and filter
-        // TODO: display result on web page, performSearch()
+        //Get query typed by user
+        String query = req.getParameter("query");
+        
+        //Check if query is empty
+        if (query.length()==0) {
+        	return;
+        }
+        
+        String[] filterList = req.getParameterValues("filters");
+        StringBuilder results;
+        try {
+			results = performSearch(query, filterList);
+	        Gson gson = new Gson();
+	        resp.setContentType("application/json");
+	        String jsonResponse = gson.toJson(results);
+	        
+	        PrintWriter output = resp.getWriter();
+	        output.print(jsonResponse);
+	        output.flush();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
     }
-
+    
     /* Perform searching, return search result (change void)
      * If we want more info than just file name in the search result,
      * then search result return in SearchResult type
      *
      * Assume the query and filter we get are all in string type for now
      */
-    public String performSearch (String queryString, String fileType, String uploader) {
-        return null;
-        //TODO
+    public StringBuilder performSearch (String queryString, String[] filterString) throws ParseException {
+    	
+    	ArrayList<String> searchFilters = new ArrayList<String>();
+    	ArrayList<String> searchQuery = new ArrayList<String>();
+    	searchQuery.add(queryString);
+        filterDecoder(filterString, searchQuery, searchFilters);
+        
+        IndexHandler handler = IndexHandler.getInstance();
+        String [] queryList = new String[searchQuery.size()];
+        queryList = searchQuery.toArray(queryList);
+        String [] filterList = new String[searchFilters.size()];
+        filterList = searchQuery.toArray(filterList);
+        
+        DocFile[] docFileResults;
+		
+		docFileResults = handler.search(queryList, filterList, true);
+		
+        StringBuilder results = new StringBuilder("Found: ");
+        
+        for (DocFile result:docFileResults) {
+        	results.append(result);
+        }
+        
+		return results;
+    	
+    }
+    	
+    private static void filterDecoder(String[] fString, ArrayList<String> searchFilters, ArrayList<String> sQuery) {
+    		
+       	searchFilters.add("Content");
+        	
+       	for (String filterType:fString) {
+       		if (filterType.equals("ePdf")) {
+       			sQuery.add(".pdf");
+       			if (!searchFilters.contains(Constants.INDEX_KEY_TYPE)) {
+       				searchFilters.add(Constants.INDEX_KEY_TYPE);
+       			}
+       		} else if (filterType.equals("eTxt")){
+       			sQuery.add(".txt");
+       			if (!searchFilters.contains(Constants.INDEX_KEY_TYPE)) {
+       				searchFilters.add(Constants.INDEX_KEY_TYPE);
+       			}
+       		} else if (filterType.equals("eHtml")) {
+       			sQuery.add(".html");
+       			if (!searchFilters.contains(Constants.INDEX_KEY_TYPE)) {
+       				searchFilters.add(Constants.INDEX_KEY_TYPE);
+       			}
+       		} else if (filterType.equals("eDocx")) {
+       			sQuery.add(".docx");
+       			if (!searchFilters.contains(Constants.INDEX_KEY_TYPE)) {
+       				searchFilters.add(Constants.INDEX_KEY_TYPE);
+       			}
+       		}
+       	}
+    	
+    	
     }
 
 }
