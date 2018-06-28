d52afa920f35cc83867d4bc47abd6adb9a30087b
Chris Chow <chrisa.chow@mail.utoronto.ca>
2018-06-28 16:58:55 -0400

    TEAM5-1 added new default file path behaviour for upload

--- src/main/java/fall2018/cscc01/team5/searchEngineWebApp/util/Constants.java
+++ src/main/java/fall2018/cscc01/team5/searchEngineWebApp/util/Constants.java
@@ -7,6 +7,9 @@ public class Constants {
     public static final String FILETYPE_PDF = "pdf";
     public static final String FILETYPE_TXT = "txt";
 
+    public static final String INDEX_UPLOAD_DIRECTORY = "uploads";
+
+    
     public static final String INDEX_KEY_CONTENT = "Content";
     public static final String INDEX_KEY_FILENAME = "Filename";
     public static final String INDEX_KEY_OWNER = "Owner";
