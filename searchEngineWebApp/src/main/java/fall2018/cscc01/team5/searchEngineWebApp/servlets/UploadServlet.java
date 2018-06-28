c95e711bc0b01e1b88c1871db7ba3bb346ebf103
Emily Huiyi Liao <hui.liao@mail.utoronto.ca>
2018-06-28 14:00:02 -0400

    TEAM5-54 Upload uploadservlet.

--- searchEngineWebApp/src/main/java/fall2018/cscc01/team5/searchEngineWebApp/servlets/UploadServlet.java
+++ searchEngineWebApp/src/main/java/fall2018/cscc01/team5/searchEngineWebApp/servlets/UploadServlet.java
@@ -1,11 +1,34 @@
 package fall2018.cscc01.team5.searchEngineWebApp.servlets;
 
-public class UploadServlet {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+public class UploadServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] args) {
 		
 	}
 	
-	
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    response.setContentType("text/html;charset=UTF-8");
+	    
+	    // request file input
+	    final String path = request.getParameter("filePath");
+	    final String fileName = request.getParameter("fileName");
+	    final Part filePart = request.getPart("file");  //return collection of all part objects 
+	    final InputStream filecontent = filePart.getInputStream();  //get the content of this part as InputStream
+
+	    
+	}
 
 }
