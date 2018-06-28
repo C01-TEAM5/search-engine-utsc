5f6ff56e455e0ba7c463d9fd31e3cfe070caf186
Kalindu De Costa <kalindu.decosta@mail.utoronto.ca>
2018-06-28 17:14:36 -0400

    TEAM5-1 enabled file saving to the server

--- src/main/java/fall2018/cscc01/team5/searchEngineWebApp/util/Constants.java
+++ src/main/java/fall2018/cscc01/team5/searchEngineWebApp/util/Constants.java
@@ -1,11 +1,15 @@
 package fall2018.cscc01.team5.searchEngineWebApp.util;
 
+import java.io.File;
+
 public class Constants {
 
     public static final String FILETYPE_DOCX = "docx";
     public static final String FILETYPE_HTML = "html";
     public static final String FILETYPE_PDF = "pdf";
     public static final String FILETYPE_TXT = "txt";
+
+    public static final String FILE_UPLOAD_PATH = "file-uploads" + File.separator;
 
     public static final String INDEX_KEY_CONTENT = "Content";
     public static final String INDEX_KEY_FILENAME = "Filename";
