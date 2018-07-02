70e215a30d5e1de3683d7979c99dba49d42dfa60
Andrew Gerrard <andrew.gerrard@mail.utoronto.ca>
2018-07-02 12:56:43 -0400

    Organized css folder, created stub for SignUp and updated route

---
+++ searchEngineWebApp/src/main/java/fall2018/cscc01/team5/searchEngineWebApp/servlets/SignUpServlet.java
@@ -0,0 +1,31 @@
+package fall2018.cscc01.team5.searchEngineWebApp.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SignUpServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public SignUpServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.getWriter().append("Served at: ").append(req.getContextPath());
+	}
+
+
+    @Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(req, resp);
+	}
+
+}
