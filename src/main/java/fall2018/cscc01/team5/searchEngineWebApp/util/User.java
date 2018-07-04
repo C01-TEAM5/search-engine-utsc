fe279ec717a6d508aa098b5557bc1c02d1995593
Emily Huiyi Liao <hui.liao@mail.utoronto.ca>
2018-07-03 21:15:30 -0400

    TEAM5-61 Create user and validate class stubs.

---
+++ src/main/java/fall2018/cscc01/team5/searchEngineWebApp/util/User.java
@@ -0,0 +1,66 @@
+package fall2018.cscc01.team5.searchEngineWebApp.util;
+
+import fall2018.cscc01.team5.searchEngineWebApp.handlers.UserValidator;
+
+public class User {
+ 
+	private String email; 
+	private String name; 
+	private String username;
+	private byte[] salt;
+	private String hash;
+	
+	public User(String username, String email, String name, String password) {
+		this.username = username; 
+		this.email = email; 
+		this.name = name; 
+		this.salt = UserValidator.getSalt();
+		this.hash = UserValidator.getSecurePassword(password, null);
+	}
+	
+	/**
+	 * Get user name
+	 * @return  username
+	 */
+	public String getUsername() {
+		return username;
+	}
+	
+	
+	/**
+	 * Get email
+	 * @return email
+	 */
+	public String getEmail() {
+		return this.email;
+	}
+	
+	
+	/**
+	 * get Name
+	 * @return name
+	 */
+	public String getName() {
+		return this.name;
+	}
+	
+	
+	/**
+	 * get Salt, the random data that append to password before
+	 * obtaining hash.
+	 * @return salt
+	 */
+	public byte[] getSalt() {
+		return this.salt;
+	}
+	
+	
+	/**
+	 * get secure password/hash
+	 * @return hash
+	 */
+	public String getHash() {
+		return this.hash;
+	}
+}
+
