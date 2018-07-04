04f3700d9a6c15989a61be39c9fd897246224167
Emily Huiyi Liao <hui.liao@mail.utoronto.ca>
2018-07-03 21:15:30 -0400

    TEAM5-61 Create user and validate class stubs.

---
+++ searchEngineWebApp/src/main/java/fall2018/cscc01/team5/searchEngineWebApp/handlers/UserValidator.java
@@ -0,0 +1,59 @@
+package fall2018.cscc01.team5.searchEngineWebApp.handlers;
+
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+import java.security.spec.InvalidKeySpecException;
+import java.security.NoSuchAlgorithmException;
+
+public class UserValidator {
+	
+
+	public UserValidator () {
+		
+	}
+
+	/**
+	 * Generate a random salt, a random data that append to password before
+	 * obtaining hash.
+	 * 
+	 * @return a random salt
+	 * @throws NoSuchAlgorithmException
+	 */
+	public static byte[] getSalt() {
+		//TODO
+	    return null;
+	}
+
+	
+	/**
+	 * Generate a secure password/hash.
+	 * 
+	 * @param password the original password to hash
+	 * @param salt a random data that append to password before hash
+	 * @return a secure password/hash
+	 */
+	public static String getSecurePassword(String password, byte[] salt) {
+		//TODO
+		return null;
+	}
+
+	
+	/**
+	 * Validate the input password is correct.
+	 * 
+	 * @param originalPassword    the input password need to validate
+	 * @param storedPassword   the hash password in database
+	 * @return  true if originalPassword is the same as storedPassword
+	 * @throws NoSuchAlgorithmException
+	 * @throws InvalidKeySpecException
+	 */
+    public static boolean validatePassword(String originalPassword, String storedPassword) {
+    	//TODO
+    	return true;
+    }
+   
+        
+}
