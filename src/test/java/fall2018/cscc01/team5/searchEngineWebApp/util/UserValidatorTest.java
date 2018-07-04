0a3f039a918479ec2dfb8cbff05cd4b4e8629b07
Andrew Gerrard <andrew.gerrard@mail.utoronto.ca>
2018-07-04 12:56:37 -0400

    TEAM5-13 Finished prototype for UserValidator

--- src/test/java/fall2018/cscc01/team5/searchEngineWebApp/util/UserValidatorTest.java
+++ src/test/java/fall2018/cscc01/team5/searchEngineWebApp/util/UserValidatorTest.java
@@ -15,13 +15,17 @@ public class UserValidatorTest {
     public void testHash() throws DecoderException, NoSuchAlgorithmException, InvalidKeySpecException {
         
         String salt = "123456789ABCDEF123456789ABCDEF";
+        String salt2 = "A23456789ABCDEF123456789ABCDEF";
         byte[] testSalt = Hex.decodeHex(salt.toCharArray());
+        byte[] testSalt2 = Hex.decodeHex(salt2.toCharArray());
+        
         String password = "password";
         
         String hashedPassword = UserValidator.getSaltedHash(password, testSalt);
+        String hashedPassword2 = UserValidator.getSaltedHash("password", testSalt2);
         
         assertEquals(false,UserValidator.validatePassword("", hashedPassword));
-        assertEquals(true,UserValidator.validatePassword("", hashedPassword));
+        assertEquals(true,UserValidator.validatePassword("password", hashedPassword));
         
         
     }
