4a622a1b693acb0dcc4d7d3860eab11ceb7c88ee
Kalindu De Costa <kalindu.decosta@mail.utoronto.ca>
2018-07-05 15:46:52 -0400

    TEAM5-13 added api to call register route

--- searchEngineWebApp/src/main/webapp/js/login.js
+++ searchEngineWebApp/src/main/webapp/js/login.js
@@ -15,3 +15,27 @@ window.onclick = function(event) {
         popupregister.style.display = "none";
     }
 }
+
+document.getElementById("RIB").addEventListener("submit", function(e){
+    e.preventDefault();
+});
+document.getElementById("SIB").addEventListener("submit", function(e){
+    e.preventDefault();
+});
+
+
+document.getElementById("RC").addEventListener("click", function(){
+    var username = document.getElementById("username").value;
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("pwd1").value;
+    console.log("success", username, name, email, password);
+    api.register(username, name, email, password, function(err, result) {
+        if (err != null) {
+            console.log(err);
+        }
+        else {
+            console.log(result);
+        }
+    });
+});
