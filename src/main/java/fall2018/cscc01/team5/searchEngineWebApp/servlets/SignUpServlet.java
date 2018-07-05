ddb3f721ef1af9fc1d8fc82d52a254eed26dfe65
Chris Chow <chrisa.chow@mail.utoronto.ca>
2018-07-05 13:47:58 -0400

    TEAM5-69 added do post for user to register new user

--- src/main/java/fall2018/cscc01/team5/searchEngineWebApp/servlets/SignUpServlet.java
+++ src/main/java/fall2018/cscc01/team5/searchEngineWebApp/servlets/SignUpServlet.java
@@ -7,9 +7,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fall2018.cscc01.team5.searchEngineWebApp.users.User;
+
 public class SignUpServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	private User user;
+	
+	
     public SignUpServlet() {
         super();
         // TODO Auto-generated constructor stub
@@ -24,8 +29,13 @@ public class SignUpServlet extends HttpServlet {
 
     @Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(req, resp);
+		try {
+		    if (user != null) {
+	            register(user);   
+		    }  
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
 	}
 
 }
