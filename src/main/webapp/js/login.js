4caef5ad65e16f19b0e80514f2dcfd0962f59671
Chris Chow <chrisa.chow@mail.utoronto.ca>
2018-07-05 22:21:29 -0400

    updated password matching function to work both ways on entry

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -54,11 +54,11 @@ document.getElementById("RC").addEventListener("click", function(){
 var check = function() {
     if (document.getElementById("pwd1").value == document.getElementById("pwd2").value) {
         document.getElementById('message').style.color = 'green';
-        document.getElementById('message').innerHTML = 'matching';
+        document.getElementById('message').innerHTML = 'passwords match';
         document.getElementById('RC').disabled = false;
     } else {
         document.getElementById('message').style.color = 'red';
-        document.getElementById('message').innerHTML = 'Must match the previous entry';
+        document.getElementById('message').innerHTML = 'The provided passwords do not match';
         document.getElementById('RC').disabled = true;
     }
 }
