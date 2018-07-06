ead175c9eca83e678f7d50ea7c7b52d832ec83db
Chris Chow <chrisa.chow@mail.utoronto.ca>
2018-07-05 22:03:21 -0400

    TEAM5-69 added new signup servlet

--- searchEngineWebApp/src/main/java/fall2018/cscc01/team5/searchEngineWebApp/servlets/SignUpServlet.java
+++ searchEngineWebApp/src/main/java/fall2018/cscc01/team5/searchEngineWebApp/servlets/SignUpServlet.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import java.util.HashMap;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,29 +19,21 @@ import fall2018.cscc01.team5.searchEngineWebApp.users.User;
 import fall2018.cscc01.team5.searchEngineWebApp.handlers.AccountManager;
 import fall2018.cscc01.team5.searchEngineWebApp.util.Constants;
 
+@WebServlet("/register")
 public class SignUpServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-	private User user;
-	
-	
+    private static final long serialVersionUID = 1L;
+    
     public SignUpServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     @Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        
-	}
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {  
+    }
     
-
-
     @Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
         Gson gson = new Gson();
         
         StringBuilder sb = new StringBuilder();
@@ -50,17 +43,21 @@ public class SignUpServlet extends HttpServlet {
         }   
         
         Map<String, String> map = gson.fromJson(sb.toString(), new TypeToken<HashMap<String, String>>() {}.getType());
-        
-        System.out.println(map.get("registerpsw"));
+
+        System.out.println(map.get("username"));
+        System.out.println(map.get("email"));
+        System.out.println(map.get("name"));
+        System.out.println(map.get("password"));
         
         // makes new user assuming list order
         // username, email, name, password
+        User user = null;
         try {
-            user = new User(
-                    map.get("registerusername"),
-                    map.get("registeruemail"), 
-                    map.get("registerusername"), 
-                    map.get("regsiterpsw"));
+           user = new User(
+                    map.get("username"),
+                    map.get("email"), 
+                    map.get("name"), 
+                    map.get("password"));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -68,22 +65,22 @@ public class SignUpServlet extends HttpServlet {
         
         // registration if not null
         try {
-		    if (user != null) {
-	            boolean success = AccountManager.register(user);
-	            if (success) {    	                
-    	            Cookie cookie = new Cookie(Constants.CURRENT_USER, user.getName());
-    	            resp.addCookie(cookie);
+            if (user != null) {
+                boolean success = AccountManager.register(user);
+                if (success) {                      
+                    Cookie cookie = new Cookie(Constants.CURRENT_USER, user.getName());
+                    resp.addCookie(cookie);
                 }
-		    }  
-		} catch (Exception e) {
-		    e.printStackTrace();
-		}
+            }  
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         
         
         // successfully signed up
         PrintWriter output = resp.getWriter();
         output.print(gson.toJson("Success"));
         output.flush();
-	}
+    }
 
 }
