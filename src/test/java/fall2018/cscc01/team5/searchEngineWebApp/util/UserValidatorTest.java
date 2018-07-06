f24beae87f772bf7000920d459cfb24afbfa9174
Andrew Gerrard <andrew.gerrard@mail.utoronto.ca>
2018-07-06 09:32:16 -0400

    TEAM5-72 add tests for valid and invalid passwords

--- src/test/java/fall2018/cscc01/team5/searchEngineWebApp/util/UserValidatorTest.java
+++ src/test/java/fall2018/cscc01/team5/searchEngineWebApp/util/UserValidatorTest.java
@@ -11,23 +11,59 @@ import org.junit.Test;
 
 public class UserValidatorTest {
 
+    /* Test valid password entry
+     * Tests include with characters, numbers, symbols
+     */
     @Test
-    public void testHash() throws DecoderException, NoSuchAlgorithmException, InvalidKeySpecException {
+    public void testValidHash() throws DecoderException, NoSuchAlgorithmException, InvalidKeySpecException {
         
         String salt = "123456789ABCDEF123456789ABCDEF";
-        String salt2 = "A23456789ABCDEF123456789ABCDEF";
-        byte[] testSalt = Hex.decodeHex(salt.toCharArray());
-        byte[] testSalt2 = Hex.decodeHex(salt2.toCharArray());
-        
+        byte[] testSalt = Hex.decodeHex(salt.toCharArray());     
         String password = "password";
-        
-        String hashedPassword = UserValidator.getSaltedHash(password, testSalt);
-        String hashedPassword2 = UserValidator.getSaltedHash("password", testSalt2);
-        
-        assertEquals(false,UserValidator.validatePassword("", hashedPassword));
+        String hashedPassword = UserValidator.getSaltedHash(password, testSalt);        
         assertEquals(true,UserValidator.validatePassword("password", hashedPassword));
         
+        salt = "123456789ABCDEF123456789123345";
+        testSalt = Hex.decodeHex(salt.toCharArray());     
+        password = "test123";
+        hashedPassword = UserValidator.getSaltedHash(password, testSalt);      
+        assertEquals(true,UserValidator.validatePassword("test123", hashedPassword));
         
+        salt = "AAA456789ABCDEF123456789123345";
+        testSalt = Hex.decodeHex(salt.toCharArray());     
+        password = "!tEst";
+        hashedPassword = UserValidator.getSaltedHash(password, testSalt);      
+        assertEquals(true,UserValidator.validatePassword("!tEst", hashedPassword));
     }
     
+    /* Test invalid passwords
+     */
+    @Test
+    public void testInvalidHash() throws DecoderException, NoSuchAlgorithmException, InvalidKeySpecException {
+        
+        String salt = "123456789ABCDEF123456789ABCDEF";
+        byte[] testSalt = Hex.decodeHex(salt.toCharArray());     
+        String password = "password";
+        String hashedPassword = UserValidator.getSaltedHash(password, testSalt);        
+        assertEquals(false,UserValidator.validatePassword("passwor", hashedPassword));
+        assertEquals(false,UserValidator.validatePassword("pass", hashedPassword));
+        assertEquals(false,UserValidator.validatePassword("hellothe", hashedPassword));
+        
+        salt = "123456789ABCDEF123456789123345";
+        testSalt = Hex.decodeHex(salt.toCharArray());     
+        password = "test123";
+        hashedPassword = UserValidator.getSaltedHash(password, testSalt);      
+        assertEquals(false,UserValidator.validatePassword("test1234", hashedPassword));
+        assertEquals(false,UserValidator.validatePassword("test", hashedPassword));
+        assertEquals(false,UserValidator.validatePassword("TEST123", hashedPassword));
+        assertEquals(false,UserValidator.validatePassword("t!st123", hashedPassword));
+        
+        salt = "AAA456789ABCDEF123456789123345";
+        testSalt = Hex.decodeHex(salt.toCharArray());     
+        password = "!tEst";
+        hashedPassword = UserValidator.getSaltedHash(password, testSalt);      
+        assertEquals(false,UserValidator.validatePassword("", hashedPassword));
+        assertEquals(false,UserValidator.validatePassword("1tEst", hashedPassword));
+        assertEquals(false,UserValidator.validatePassword("1tEs", hashedPassword));
+    }
 }
