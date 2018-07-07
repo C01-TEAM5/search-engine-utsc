31ae6fbbf944a1e6a4cd2bc56a9d66fbf4e3a2a9
Kalindu De Costa <kalindu.decosta@mail.utoronto.ca>
2018-07-07 18:05:13 -0400

    TEAM5-13 fixed the login form not closing

--- searchEngineWebApp/src/main/webapp/js/login.js
+++ searchEngineWebApp/src/main/webapp/js/login.js
@@ -1,21 +1,24 @@
-
 var popuplogin = document.getElementById('SIB');
-
-window.onclick = function(event) {
-    if (event.target == popuplogin) {
-        popuplogin.style.display = "none";
-    }
-}
-
-
 var popupregister = document.getElementById('RIB');
 
 window.onclick = function(event) {
-
-    if (event.target == popupregister) {
+    if (event.target == popuplogin || event.target == popupregister) {
         popupregister.style.display = "none";
+        popuplogin.style.display = "none";
+        history.pushState(null, '', '/');
     }
 }
+
+// handle login and register buttons
+$(".registerButton").click(function(){
+    console.log(document.URL);
+    showRegister();
+});
+
+$(".loginButton").click(function(){
+    console.log(document.URL);
+    showLogin();
+});
 
 document.getElementById("RIB").addEventListener("submit", function(e){
     e.preventDefault();
@@ -84,3 +87,23 @@ var check = function() {
         document.getElementById('RC').disabled = true;
     }
 }
+
+var showLogin = function() {
+    history.pushState(null, '', '?login');
+    document.getElementById('SIB').style.display='block';
+}
+
+var showRegister = function() {
+    history.pushState(null, '', '?register');
+    document.getElementById('RIB').style.display='block';
+}
+
+var parseURL =  function() {
+    var urlParts = document.URL.split("?");
+    if(urlParts.length > 1) {
+        if (urlParts[1] === "login") showLogin();
+        if (urlParts[1] === "register") showRegister();
+    }
+}
+
+parseURL();
