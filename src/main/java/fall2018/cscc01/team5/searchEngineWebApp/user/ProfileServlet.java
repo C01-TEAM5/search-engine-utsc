c890202f99380b76094f8e6efce9371ade116211
Kalindu De Costa <kalindu.decosta@mail.utoronto.ca>
2018-07-12 18:13:00 -0400

    TEAM5-97 progress on profile and courses

--- src/main/java/fall2018/cscc01/team5/searchEngineWebApp/user/ProfileServlet.java
+++ src/main/java/fall2018/cscc01/team5/searchEngineWebApp/user/ProfileServlet.java
@@ -2,6 +2,7 @@ package fall2018.cscc01.team5.searchEngineWebApp.user;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,5 +19,15 @@ public class ProfileServlet extends HttpServlet {
         
         RequestDispatcher view = req.getRequestDispatcher("profile.html");
         view.forward(req, resp);
+    }
+
+    private String getCurrentUser(Cookie[] cookies) {
+        String res = "";
+        if (cookies == null) return res;
+        for (Cookie cookie: cookies) {
+            if (cookie.getName().equals("currentUser")) res = cookie.getValue();
+        }
+
+        return res;
     }
 }
