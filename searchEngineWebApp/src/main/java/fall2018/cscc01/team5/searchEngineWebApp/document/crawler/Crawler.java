178c70d1099e890d715e985ee4532b713978c652
Andrew <argerrard@live.com>
2018-07-24 10:22:12 -0400

    TEAM5-123 added shouldVisit class to Crawler

--- searchEngineWebApp/src/main/java/fall2018/cscc01/team5/searchEngineWebApp/document/crawler/Crawler.java
+++ searchEngineWebApp/src/main/java/fall2018/cscc01/team5/searchEngineWebApp/document/crawler/Crawler.java
@@ -1,5 +1,74 @@
 package fall2018.cscc01.team5.searchEngineWebApp.document.crawler;
 
-public class Crawler {
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Pattern;
+import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
+import edu.uci.ics.crawler4j.url.WebURL;
 
+public class Crawler extends WebCrawler {
+
+    //Adjusted filters from the documentation to suit my needs
+    private static final Pattern acceptedTypes = Pattern.compile(
+            ".*(\\.(pdf|txt|docx|html))$");
+
+    /**
+     * Method to determine if a given page should be visited
+     * by the crawler. Returns true if it should be visited,
+     * false otherwise.
+     * 
+     */
+    @Override
+    public boolean shouldVisit(Page referringPage, WebURL url) {
+    	
+    	String href = url.getURL().toLowerCase();
+    	
+    	try {
+			boolean inSeeds = withinSeeds(referringPage, url);
+			return inSeeds && acceptedTypes.matcher(href).matches();
+		} catch (MalformedURLException e) {
+			return false;
+		}
+    	
+    }
+    
+    /**
+     * This method is called when a page is visited. Responsible for
+     * gathering the required information and sending to the CSVWriter
+     * to be written to file.
+     * 
+     */
+    @Override
+    public void visit(Page page) {
+        
+      
+    }
+    
+    
+    /**
+     * Helper function to determine if a given url is within
+     * the seed of the referring page. We want to keep all crawled
+     * results within the original website where the crawl started
+     * 
+     * @param referringPage the page we came from (we want to stay in this seed)
+     * @param url the url we have found
+     * @return true if it is within the seed, false otherwise
+     * @throws MalformedURLException
+     */
+    private boolean withinSeeds(Page referringPage, WebURL url) throws MalformedURLException {
+        
+        boolean withinSeed = false;
+        URL newURL = new URL(url.getURL().toLowerCase());
+        WebURL seedWebURL = referringPage.getWebURL();
+        URL seedURL = new URL(seedWebURL.getURL().toLowerCase());
+        
+        if (seedURL.getHost().equals(newURL.getHost())) {
+            withinSeed = true;
+        }
+         
+        return withinSeed;
+        
+    }
 }
--- searchEngineWebApp/src/main/java/fall2018/cscc01/team5/searchEngineWebApp/document/crawler/CrawlerController.java
+++ searchEngineWebApp/src/main/java/fall2018/cscc01/team5/searchEngineWebApp/document/crawler/CrawlerController.java
@@ -1,5 +1,61 @@
 package fall2018.cscc01.team5.searchEngineWebApp.document.crawler;
 
-public class CrawlerController {
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 
+/**
+ * Crawler Controller used to establish the correct
+ * settings for a document crawl. Also used to start the
+ * crawl once everything has been setup.
+ */
+public class CrawlerController {
+	
+	private CrawlController controller;
+	private String seed;
+	private int depth;
+	private int pages;
+
+	/**
+	 * Create a new CrawlerController with the target seed, 
+	 * depth and pages to check.
+	 * 
+	 * @param seed the seed that the crawler will crawl
+	 * @param depth the depth that the crawler will crawl
+	 * @param pages the max number of pages the crawler will crawl
+	 * @throws Exception 
+	 */
+	public CrawlerController(String seed, int depth, int pages) throws Exception {
+		
+		this.seed = seed;
+		this.depth = depth;
+		this.pages = pages;
+		
+		CrawlConfig config = new CrawlConfig();
+		config.setMaxDepthOfCrawling(depth);
+		config.setMaxPagesToFetch(pages);
+		config.setIncludeBinaryContentInCrawling(true);
+		
+		//Create the controller for the crawl
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        
+        this.controller = new CrawlController(config, pageFetcher, robotstxtServer);
+        this.controller.addSeed(seed);
+		
+	}
+	
+	/**
+	 * Starts the crawl with the given controller setup.
+	 * 
+	 */
+	public void startCrawl() {
+		
+		controller.start(Crawler.class, 1);
+		
+	}	
+	
 }
