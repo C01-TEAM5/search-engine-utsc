90c291783ac61f13e99199681ce94bbca0d2734d
Andrew Gerrard <andrew.gerrard@mail.utoronto.ca>
2018-07-27 00:24:41 -0400

    TEMP5-124 adjusted crawler to work with new cookies

--- searchEngineWebApp/src/test/java/fall2018/cscc01/team5/searchEngineWebApp/user/AccountManagerTest.java
+++ searchEngineWebApp/src/test/java/fall2018/cscc01/team5/searchEngineWebApp/user/AccountManagerTest.java
@@ -1,5 +1,5 @@
 package fall2018.cscc01.team5.searchEngineWebApp.user;
-/*
+
 import static org.junit.Assert.fail;
 
 import java.security.NoSuchAlgorithmException;
@@ -108,4 +108,4 @@ public class AccountManagerTest {
             Assert.assertEquals(AccountManager.getPermission(users[i].getUsername()), Constants.PERMISSION_INSTRUCTOR);
         }
     }
-}*/
+}
