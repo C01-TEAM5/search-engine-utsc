e262ec144623d39f0b7a2bd1e9844e2c20c3d47c
Emily Huiyi Liao <hui.liao@mail.utoronto.ca>
2018-07-29 17:35:30 -0400

    TEAM5-114 fix filter

--- searchEngineWebApp/src/main/webapp/js/filter.js
+++ searchEngineWebApp/src/main/webapp/js/filter.js
@@ -9,12 +9,13 @@ console.log(type);
 google.charts.load('current', {'packages':['corechart']});
 
 // Set a callback to run when the Google Visualization API is loaded.
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawChart1);
+google.charts.setOnLoadCallback(drawChart2);
 
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
 // draws it.
-function drawChart() {
+function drawChart1() {
 
   // File Type
   // Create the data table.
@@ -64,6 +65,9 @@ function drawChart() {
   google.visualization.events.addListener(fileTypechart, 'select', selectTypeHandler);
   fileTypechart.draw(fileTyepData, fileTypeoptions);
 
+}
+
+function drawChart2() {
 
   // File perm
   // Create the data table.
@@ -95,98 +99,99 @@ function drawChart() {
       document.getElementById("search").value = query;
       if (parseInt($("#docxNum").html()) > 0) {
             document.getElementById("searchDocx").checked = true;
-      } else  if (parseInt($("#htmlNum").html()) > 0) {
+      }
+      if (parseInt($("#htmlNum").html()) > 0) {
             document.getElementById("searchHtml").checked = true;
-      } else if (parseInt($("#pdfNum").html()) > 0) {
+      }
+      if (parseInt($("#pdfNum").html()) > 0) {
             document.getElementById("searchPdf").checked = true;
-      } else  if (parseInt($("#txtNum").html()) > 0) {
+      }
+      if (parseInt($("#txtNum").html()) > 0) {
             document.getElementById("searchTxt").checked = true;
       }
       document.getElementById("submit").click();
   }
   google.visualization.events.addListener(filePermchart, 'select', selectPermHandler);
   filePermchart.draw(filePermData, filePermoptions);
-
-
-  //File Owner
-  // var fileOwnerData = new google.visualization.DataTable();
-  // fileOwnerData.addColumn('string', 'File Owner');
-  // fileOwnerData.addColumn('number', 'Occurence');
-  // var OwnerTable = document.getElementById('OwnerData');
-  // for (var i=0; i<OwnerTable.rows.length; i++) {
-  //      fileOwnerData.addRow([OwnerTable.rows[i].cells[0].innerHTML, parseInt(OwnerTable.rows[i].cells[1].innerHTML)]);
-  // }
-  // var len = OwnerTable.rows.length;
-  // // console.log(len);
-  // var fileOwneroptions = {'title' : 'File Owner Statistics', 'width':300, 'height':180};
-  // var fileOwnerchart = new google.visualization.PieChart(document.getElementById('fileOwnerFilter'));
-  // function selectOwnerHandler() {
-  //     var selectOwner = fileOwnerchart.getSelection()[0];
-  //     if (selectOwner) {
-  //         var owner = fileOwnerData.getValue(selectOwner.row, 0);
-  //         document.getElementById("search").value = query+ ":owner:" + owner;
-  //     }
-  //     // owner type
-  //     if (perm=="0") {
-  //         document.getElementById("perm-all").click();
-  //     } else if (perm=="3") {
-  //         document.getElementById("perm-instructor").click();
-  //     } else if (perm=="2") {
-  //         document.getElementById("perm-student").click();
-  //     }
-  //     // file type
-  //     if (parseInt($("#docxNum").html()) > 0) {
-  //           document.getElementById("searchDocx").checked = true;
-  //     } else  if (parseInt($("#htmlNum").html()) > 0) {
-  //           document.getElementById("searchHtml").checked = true;
-  //     } else if (parseInt($("#pdfNum").html()) > 0) {
-  //           document.getElementById("searchPdf").checked = true;
-  //     } else  if (parseInt($("#txtNum").html()) > 0) {
-  //           document.getElementById("searchTxt").checked = true;
-  //     }
-  //     document.getElementById("submit").click();
-  // }
-  // google.visualization.events.addListener(fileOwnerchart, 'select', selectOwnerHandler);
-  // fileOwnerchart.draw(fileOwnerData, fileOwneroptions);
-
-
-  //Course Code
-  // var courseData = new google.visualization.DataTable();
-  // courseData.addColumn('string', 'Course Code');
-  // courseData.addColumn('number', 'Occurence');
-  // var CourseTable = document.getElementById('CourseData');
-  // for (var i=0; i<CourseTable.rows.length; i++) {
-  //      courseData.addRow([CourseTable.rows[i].cells[0].innerHTML, parseInt(CourseTable.rows[i].cells[1].innerHTML)]);
-  // }
-  // var courseoptions = {'title' : 'Course Code Statistics', 'width':300, 'height':180};
-  // var coursechart = new google.visualization.PieChart(document.getElementById('courseCodeFilter'));
-  // function selectCourseHandler() {
-  //     var selectCourse = coursechart.getSelection()[0];
-  //     if (selectCourse) {
-  //         var course = courseData.getValue(selectCourse.row, 0);
-  //         document.getElementById("search").value = query+ ":courseCode:" + course;
-  //     }
-  //     // owner type
-  //     if (perm=="0") {
-  //         document.getElementById("perm-all").click();
-  //     } else if (perm=="3") {
-  //         document.getElementById("perm-instructor").click();
-  //     } else if (perm=="2") {
-  //         document.getElementById("perm-student").click();
-  //     }
-  //     // file type
-  //     if (parseInt($("#docxNum").html()) > 0) {
-  //           document.getElementById("searchDocx").checked = true;
-  //     } else  if (parseInt($("#htmlNum").html()) > 0) {
-  //           document.getElementById("searchHtml").checked = true;
-  //     } else if (parseInt($("#pdfNum").html()) > 0) {
-  //           document.getElementById("searchPdf").checked = true;
-  //     } else  if (parseInt($("#txtNum").html()) > 0) {
-  //           document.getElementById("searchTxt").checked = true;
-  //     }
-  //     document.getElementById("submit").click();
-  // }
-  // google.visualization.events.addListener(coursechart, 'select', selectCourseHandler);
-  // coursechart.draw(courseData, courseoptions);
-
 }
+
+//File Owner
+// var fileOwnerData = new google.visualization.DataTable();
+// fileOwnerData.addColumn('string', 'File Owner');
+// fileOwnerData.addColumn('number', 'Occurence');
+// var OwnerTable = document.getElementById('OwnerData');
+// for (var i=0; i<OwnerTable.rows.length; i++) {
+//      fileOwnerData.addRow([OwnerTable.rows[i].cells[0].innerHTML, parseInt(OwnerTable.rows[i].cells[1].innerHTML)]);
+// }
+// var len = OwnerTable.rows.length;
+// // console.log(len);
+// var fileOwneroptions = {'title' : 'File Owner Statistics', 'width':300, 'height':180};
+// var fileOwnerchart = new google.visualization.PieChart(document.getElementById('fileOwnerFilter'));
+// function selectOwnerHandler() {
+//     var selectOwner = fileOwnerchart.getSelection()[0];
+//     if (selectOwner) {
+//         var owner = fileOwnerData.getValue(selectOwner.row, 0);
+//         document.getElementById("search").value = query+ ":owner:" + owner;
+//     }
+//     // owner type
+//     if (perm=="0") {
+//         document.getElementById("perm-all").click();
+//     } else if (perm=="3") {
+//         document.getElementById("perm-instructor").click();
+//     } else if (perm=="2") {
+//         document.getElementById("perm-student").click();
+//     }
+//     // file type
+//     if (parseInt($("#docxNum").html()) > 0) {
+//           document.getElementById("searchDocx").checked = true;
+//     } else  if (parseInt($("#htmlNum").html()) > 0) {
+//           document.getElementById("searchHtml").checked = true;
+//     } else if (parseInt($("#pdfNum").html()) > 0) {
+//           document.getElementById("searchPdf").checked = true;
+//     } else  if (parseInt($("#txtNum").html()) > 0) {
+//           document.getElementById("searchTxt").checked = true;
+//     }
+//     document.getElementById("submit").click();
+// }
+// google.visualization.events.addListener(fileOwnerchart, 'select', selectOwnerHandler);
+// fileOwnerchart.draw(fileOwnerData, fileOwneroptions);
+
+
+//Course Code
+// var courseData = new google.visualization.DataTable();
+// courseData.addColumn('string', 'Course Code');
+// courseData.addColumn('number', 'Occurence');
+// var CourseTable = document.getElementById('CourseData');
+// for (var i=0; i<CourseTable.rows.length; i++) {
+//      courseData.addRow([CourseTable.rows[i].cells[0].innerHTML, parseInt(CourseTable.rows[i].cells[1].innerHTML)]);
+// }
+// var courseoptions = {'title' : 'Course Code Statistics', 'width':300, 'height':180};
+// var coursechart = new google.visualization.PieChart(document.getElementById('courseCodeFilter'));
+// function selectCourseHandler() {
+//     var selectCourse = coursechart.getSelection()[0];
+//     if (selectCourse) {
+//         var course = courseData.getValue(selectCourse.row, 0);
+//         document.getElementById("search").value = query+ ":courseCode:" + course;
+//     }
+//     // owner type
+//     if (perm=="0") {
+//         document.getElementById("perm-all").click();
+//     } else if (perm=="3") {
+//         document.getElementById("perm-instructor").click();
+//     } else if (perm=="2") {
+//         document.getElementById("perm-student").click();
+//     }
+//     // file type
+//     if (parseInt($("#docxNum").html()) > 0) {
+//           document.getElementById("searchDocx").checked = true;
+//     } else  if (parseInt($("#htmlNum").html()) > 0) {
+//           document.getElementById("searchHtml").checked = true;
+//     } else if (parseInt($("#pdfNum").html()) > 0) {
+//           document.getElementById("searchPdf").checked = true;
+//     } else  if (parseInt($("#txtNum").html()) > 0) {
+//           document.getElementById("searchTxt").checked = true;
+//     }
+//     document.getElementById("submit").click();
+// }
+// google.visualization.events.addListener(coursechart, 'select', selectCourseHandler);
+// coursechart.draw(courseData, courseoptions);
