b1a10c3c5808200439339de9fbc0bd1633671021
Kalindu De Costa <kalindu.decosta@mail.utoronto.ca>
2018-07-31 13:40:23 -0400

    TEAM5-127 improved css for the results page and increased number of results to 10 per page

--- searchEngineWebApp/src/main/java/fall2018/cscc01/team5/searchEngineWebApp/document/SearchServlet.java
+++ searchEngineWebApp/src/main/java/fall2018/cscc01/team5/searchEngineWebApp/document/SearchServlet.java
@@ -47,7 +47,7 @@ public class SearchServlet extends HttpServlet {
     protected void doGet (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         
         int currentPage = 1; //current page we are on
-        int resultsPerPage = 3; //number of results to be shown per page
+        int resultsPerPage = 10; //number of results to be shown per page
         int totalResults; //total search results on all pages
         int pagesRequired; //pages required to display total search results
         
@@ -70,7 +70,6 @@ public class SearchServlet extends HttpServlet {
             filterParam = "";
         } 
         
-    	System.out.println(filterParam);
         String[] filterQuery = filterParam.split(",");
         
         //Get permission level for search. If none, search all types of docs.
